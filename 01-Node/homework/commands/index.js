1aa12a5f8818a41c1910c80ebc428a047fda0f9e
Fran <franco_kuperman@hotmail.com>
2022-12-13 06:49:50 -0300

    first commit

--- 01-Node/homework/commands/index.js
+++ 01-Node/homework/commands/index.js
@@ -1,5 +1,17 @@
+const echo = (args, print) => {
+    print(args.join(' '));
+};
 
+const pwd = (args, print) => {
+    print(__dirname.split('\\').at(-1));
+};
+
+const date = (args, print) => {
+    print(Date());
+};
 
 module.exports = {
-
-}
+    echo,
+    pwd,
+    date,
+};
