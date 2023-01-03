4a7b510e794894d15976ddc313e5618b7aab18cf
Fran <franco_kuperman@hotmail.com>
2023-01-03 02:26:19 -0300

    'modificaciones'

--- 03-WebServer/homework/Beatles/app.js
+++ 03-WebServer/homework/Beatles/app.js
@@ -21,4 +21,37 @@ var beatles=[{
   birthdate: "07/08/1940",
   profilePic:"http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg"
 }
-]
+];
+
+http.createServer((req, res) => {
+
+  const miUrl = req.url.split("/");
+  if (miUrl.length < 3) {
+    const beatleName = miUrl[1].replace("%20", " ");
+    const beatle = beatles.filter((elem) => elem.name === beatleName)[0];
+    let template = fs.readFileSync("./beatle.html", "utf-8");
+
+    template = template.replace("{name}", beatle.name);
+    template = template.replace("{birthdate}", beatle.birthdate);
+    template = template.replace("{image}", beatle.profilePic);
+
+    return res.writeHead(200, { "Content-Type": "text/html" }).end(template)
+  }
+
+  if(req.url === "/") {
+    const index = fs.readFileSync("./index.html", "utf-8");
+    return res.writeHead(200, { "Content-Type": "text/html" }).end(index)
+  }
+// **************************************************************************************************
+  if (req.url === "/api") {
+    return res.writeHead(200, { "Content-Type": "application/json" }).end(JSON.stringify(beatles))
+  }
+
+  const beatleName = req.url.split("/").pop().replace("%20", " ");
+
+  if (req.url.includes("/api") && beatleName) {
+    const beatle = beatles.filter((elem) => elem.name === beatleName);
+    if (!beatle.length) return res.writeHead(404).end("Beatle not found")
+    return res.writeHead(200, { "Content-Type": "application/json" }).end(JSON.stringify(beatle))
+  }
+}).listen(3001, 'localhost')
